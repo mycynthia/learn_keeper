8fa2b7e86c14a32b87d34b722ef3dbcb599654eb
Cynthia <ex09190919@yahoo.com.tw>
2018-09-21 14:51:44 +0800

    add user edit route and page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update] 
+  def show
+    @user = User.find(params[:id])
+  end
+  def edit
+    unless @user == current_user
+      rediredt_to user_path(@user)
+    end
+  end
+  def update
+    @user.update(user_params)
+    rediredt_to user_path
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+  def user_params
+    params.require(:user).permit(:name)
+  end
 end
