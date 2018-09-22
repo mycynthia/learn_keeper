bcbc478d396a8a901e69326d7a136944dd492733
Irake <irake99@gmail.com>
2018-09-22 18:31:39 +0800

    Add user and role seed data
    Use 'rake db:seed' to create them.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# User and role seed data
+User.create!(id: 0, name: "admin", password: "654321", email: "admin@test.com")
+User.create!(id: 1, name: "user1", password: "123456", email: "uesr1@test.com")
+
+Role.create!(id: 0, name: "admin")
+Role.create!(id: 1, name: "user")
+
+RoleRecord.create!(user_id: 0, role_id: 0)
+RoleRecord.create!(user_id: 0, role_id: 1)
+RoleRecord.create!(user_id: 1, role_id: 1)
+
