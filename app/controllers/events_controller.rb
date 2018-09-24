c6c90d2b98e99b2a9f0ff2ee80c1fba9faccf4ad
Cynthia <ex09190919@yahoo.com.tw>
2018-09-24 15:37:52 +0800

    Setup event new and create action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,3 +1,21 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!
+  def new
+    @event = Event.new
+  end
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      flash[:notice] = "Event was successfully created"
+      rediedt_to group_event_path
+    else
+      flash.now[:alert] = "Event was failed to create"
+      render :new
+    end
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:name, :description, :image, :contacts, :address, :datetime, :location)
+  end
 end
