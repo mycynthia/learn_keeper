885020b7c8700cfc36db76c0129bdd7701ee8df2
Cynthia <ex09190919@yahoo.com.tw>
2018-10-02 12:57:47 +0800

    Add location column to groups table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_02_042010) do
+ActiveRecord::Schema.define(version: 2018_10_02_045455) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2018_10_02_042010) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "location"
   end
 
   create_table "label_groups", force: :cascade do |t|
