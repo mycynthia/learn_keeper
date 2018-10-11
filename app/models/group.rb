0757b0f1357a72a361e3e5e8f671ea272d7b1d11
T.C <travis18@gmail.com>
2018-10-11 21:36:07 +0800

    setup carrierwave to group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,6 @@
 class Group < ApplicationRecord
   # validates_presence_of :name, :image, :description
+  mount_uploader :image, GroupUploader
   has_many :label_groups
   has_many :labels, through: :label_groups
 end
