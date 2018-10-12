92e57dcf95cd081eddff003eac7ffb3804e153b2
T.C <travis18@gmail.com>
2018-10-12 16:15:59 +0800

    install and setup gem simple token

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,4 +1,5 @@
 class ApiController < ActionController::Base
+  acts_as_token_authentication_handler_for User, fallback: :none
   protect_from_forgery with: :null_session
   before_action :authenticate_user!
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_05_121307) do
+ActiveRecord::Schema.define(version: 2018_10_12_075020) do
 
   create_table "Users", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2018_10_05_121307) do
     t.string "fb_token"
     t.string "google_uid"
     t.string "google_token"
+    t.string "authentication_token", limit: 30
+    t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
