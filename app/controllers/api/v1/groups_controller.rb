87e18d8cb277452f94848dc53cecd36230ef96fe
T.C <travis18@gmail.com>
2018-10-22 20:13:49 +0800

    Add group creater support in Api GroupController

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -13,10 +13,16 @@ class Api::V1::GroupsController < ApiController
     @group.image = params[:image]
     @group.location = params[:location]
     @group.description = params[:description]
+    @group.user = current_user
     groupLabels = JSON.parse(params[:selectedLabels])
     @group_labels = Array.new
     # save new group
     if @group.save! #create group labels if group created successfully
+      # add current user to group_records
+      group_record = @group.group_records.build()
+      group_record.user = current_user
+      group_record.save!
+      # add selected labels to label_groups
       groupLabels.each do |label|
         obj = Label.find(label["id"])
         if obj
